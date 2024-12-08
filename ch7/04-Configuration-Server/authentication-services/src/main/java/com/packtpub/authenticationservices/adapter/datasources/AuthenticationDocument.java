b04e5070f2a4cdfe7eeb06417ff2936fb8818b96
Wanderson Xesquevixos <wandersonxs@icloud.com>
2024-12-08 17:04:33 -0300

    Chapter 7 - Configuration Services

--- ch7/04-Configuration-Server/authentication-services/src/main/java/com/packtpub/authenticationservices/adapter/datasources/AuthenticationDocument.java
+++ ch7/04-Configuration-Server/authentication-services/src/main/java/com/packtpub/authenticationservices/adapter/datasources/AuthenticationDocument.java
@@ -7,13 +7,12 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
-import java.util.UUID;
 
 @Document(collection = "authentication")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthenticationEntity {
+public class AuthenticationDocument {
 
     @Id
     private String id;
--- ch7/04-Configuration-Server/authentication-services/src/main/java/com/packtpub/authenticationservices/config/bootstrap/BeansConfiguration.java
+++ ch7/04-Configuration-Server/authentication-services/src/main/java/com/packtpub/authenticationservices/config/bootstrap/BeansConfiguration.java
@@ -3,7 +3,7 @@ package com.packtpub.authenticationservices.config.bootstrap;
 import com.packtpub.authenticationservices.adapter.datasources.AuthenticationManagerSecurity;
 import com.packtpub.authenticationservices.adapter.datasources.TokenJwt;
 import com.packtpub.authenticationservices.adapter.datasources.UserRestApi;
-import com.packtpub.authenticationservices.config.correlation.CorrelationIdUtil;
+import com.packtpub.authenticationservices.config.logging.CustomLoadBalancerInterceptor;
 import com.packtpub.authenticationservices.internal.repositories.AuthenticationManagerRepository;
 import com.packtpub.authenticationservices.internal.usecases.GenerateTokenUseCase;
 import com.packtpub.authenticationservices.internal.usecases.ValidateTokenUseCase;
@@ -11,7 +11,6 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.web.client.RequestCallback;
 import org.springframework.web.client.RestClient;
 
 @Configuration
@@ -33,8 +32,11 @@ public class BeansConfiguration {
 
     @LoadBalanced
     @Bean
-    public RestClient.Builder restClient() {
-        return RestClient.builder();
+    public RestClient.Builder restClient(CustomLoadBalancerInterceptor customLoadBalancerInterceptor) {
+        return RestClient
+                .builder()
+                .requestInterceptor(customLoadBalancerInterceptor);
     }
+
 
 }
